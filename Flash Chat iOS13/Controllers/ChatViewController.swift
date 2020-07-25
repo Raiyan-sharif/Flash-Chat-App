b4d2cac10a175da0b481cb275241651b9cfcf151
Raiyan <raiyan.sharif.1234@gmail.com>
2020-07-25 20:54:02 +0600

    Selection on message removed

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,10 +14,19 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [ Message(sender: "1@2.com", body: "Hello"),
+                                Message(sender: "a@b.com", body: "Hey!"),
+                                Message(sender: "1@2.com", body: "What's up?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+//        tableView.delegate = self
         title = Constants.appName
         navigationItem.hidesBackButton = true
+        
+        
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -36,3 +45,22 @@ class ChatViewController: UIViewController {
           
     }
 }
+extension ChatViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
+    }
+    
+    
+}
+
+//extension ChatViewController: UITableViewDelegate{
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath.row)
+//    }
+//}
